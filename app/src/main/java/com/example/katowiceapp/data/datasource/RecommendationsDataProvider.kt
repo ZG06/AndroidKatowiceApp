a1365722731ee3f1ee4175bd1c2ef5e668010d71
ZG06 <s1demusic06@gmail.com>
2025-04-30 23:35:24 +0200

    Added more recommendations

--- app/src/main/java/com/example/katowiceapp/data/datasource/RecommendationsDataProvider.kt
+++ app/src/main/java/com/example/katowiceapp/data/datasource/RecommendationsDataProvider.kt
@@ -2,6 +2,7 @@ package com.example.katowiceapp.data.datasource
 
 import com.example.katowiceapp.R
 import com.example.katowiceapp.data.SelectableItem.RecommendationItem
+import com.example.katowiceapp.ui.RecommendationScreen
 
 object RecommendationsDataProvider {
 
@@ -21,6 +22,16 @@ object RecommendationsDataProvider {
             image = R.drawable.black_woolf_coffe_and_books,
             name = "Black Woolf Coffee & Books",
             description = "Bookstore café with a creative vibe. Perfect for relaxing with a coffee and a good read."
+        ),
+        RecommendationItem(
+            image = R.drawable.urban_coffee,
+            name = "Urban Coffee",
+            description = "Urban Coffee is a place created with Katowice in mind. In the morning, you can drop by for a good coffee and breakfast before work, and in the evening, unwind with a glass of wine."
+        ),
+        RecommendationItem(
+            image = R.drawable.kafej,
+            name = "Kafej",
+            description = "Relaxed cafe offering breakfast & lunch menus, along with trendy decor & outdoor seating."
         )
     )
 
@@ -45,6 +56,11 @@ object RecommendationsDataProvider {
             image = R.drawable.len_arte,
             name = "Len Arte",
             description = "A well-loved Italian restaurant specializing in Neapolitan-style pizzas made in a wood-fired oven. Also offers pasta, antipasti, and a relaxed yet elegant setting."
+        ),
+        RecommendationItem(
+            image = R.drawable.floor_27,
+            name = "27th Floor",
+            description = "The restaurant is located on the 27th floor of the Marriott Hotel in Katowice. From here, you can enjoy a stunning view of the Silesian metropolis and the Beskidy Mountains. However, the biggest highlight of the 27th Floor restaurant is its sensational cuisine."
         )
     )
 
@@ -64,6 +80,16 @@ object RecommendationsDataProvider {
             image = R.drawable.galeria_szyb_wilson,
             name = "Galeria Szyb Wilson",
             description = "Located in a former coal mine shaft, this is one of Poland’s largest private art galleries. It showcases contemporary art in a raw, industrial space. Also hosts festivals and events."
+        ),
+        RecommendationItem(
+            image = R.drawable.computer_museum,
+            name = "Museum of the History of Computers and Information Technology",
+            description = "Technology museum tracing the history of computing & IT in Poland, with working vintage machines."
+        ),
+        RecommendationItem(
+            image = R.drawable.walcownia,
+            name = "WALCOWNIA Zinc Metallurgy Museum",
+            description = "At the WALCOWNIA Zinc Metallurgy Museum in Katowice, visitors can see a unique, historically preserved zinc sheet production line—one of its kind in Europe. The massive, over 100-year-old machines are still used for their original purpose during occasional demonstrations. The museum also houses one of the largest collections of vintage Harley-Davidson and Indian motorcycles from 1920 to 1986."
         )
     )
 
@@ -83,6 +109,16 @@ object RecommendationsDataProvider {
             image = R.drawable.campanile_katowice,
             name = "Campanile Katowice",
             description = "Budget-friendly and practical, Campanile is about 2.5 km from the city center. Ideal for short stays, with simple rooms, decent food, and easy highway access."
+        ),
+        RecommendationItem(
+            image = R.drawable.quality_silesian_hotel,
+            name = "Quality Silesian Hotel",
+            description = "A comfortable and modern 3-star hotel located near the A4 motorway, offering convenient access to the city center and major transport routes. It features well-equipped rooms, a restaurant, sauna, and fitness area, making it a good choice for both business and leisure travelers."
+        ),
+        RecommendationItem(
+            image = R.drawable.hotel_monopol,
+            name = "Hotel Monopol",
+            description = "A luxurious 5-star hotel in the heart of Katowice, known for its elegant interiors, historic architecture, and high-end service. It offers spacious rooms, a fine dining restaurant, a wellness zone with a pool and spa, and is popular among celebrities and business guests."
         )
     )
 }
